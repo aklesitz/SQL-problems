c570acd0d09efde14534c18a59e06b84653567f0
Adam Klesitz <aklesitz@gmail.com>
2023-04-21 11:01:00 -0400

    Inner, Outer Joins, Union, Union All, and Except practice problems

--- multiple_tables.sql
+++ multiple_tables.sql
@@ -17,5 +17,24 @@ FROM regions r, employees e
 WHERE e.region_id = r.region_id
 GROUP BY 1;
 
+-- return departments in employees table that are not in departments
+SELECT DISTINCT e.department, d.department
+FROM employees e 
+LEFT JOIN departments d
+ON e.department = d.department
+WHERE d.department IS NULL;
 
+SELECT DISTINCT department
+FROM employees
+EXCEPT
+SELECT department
+from departments;
 
+-- Oracle uses MINUS instead of EXCEPT
+-- return department and total number of employees per department, total at end
+SELECT department, count(employee_id)
+FROM employees
+GROUP BY 1
+UNION ALL
+SELECT 'TOTAL', count(employee_id)
+FROM employees;
